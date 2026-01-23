95921c0b1eadc757f00a286a60a56a450fd85562
sandeep yadav <sandeep@rechargezap.in>
2026-01-23 14:19:20 +0530

    student home page with utils page customization in TeacherPageData.

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,9 +1,22 @@
 import Announcement from "@/components/Announcement";
-import BigCalender from "@/components/BigCalender";
+import BigCalenderContainer from "@/components/BigCalenderContainer";
 import EventCalander from "@/components/EventCalander";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const StudentPage = () => {
+const StudentPage = async () => {
+
+  const { userId } = await auth();
+  console.log("student userId", userId);
+
+  const classItem = await prisma.class.findMany({
+    where: {
+      students: {some: {id: userId!}},
+    },
+  });
+  console.log("student classItem", classItem);
+
   return (
     <>
       <div className="flex gap-4 flex-col md:flex-row p-4">
@@ -11,7 +24,7 @@ const StudentPage = () => {
         <div className="w-full md:w-2/3 h-full flex flex-col gap-8 bg-white rounded-md p-4">
           <div className="mb-4 ">
             <span className="text-lg font-semibold">Shedule (6A)</span>
-            <BigCalender />
+            <BigCalenderContainer type="classId" id={classItem[0].id}/>
           </div>
         </div>
         {/* right side */}
